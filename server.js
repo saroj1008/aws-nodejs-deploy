3731f71dd6de440ba199bdb275a02f522fed7cf9
saroj <sarojregmi279@gmail.com>
2023-10-08 14:05:19 -0400

    env push

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config
 
 app.get('/api/get', (req, res) => {
     res.send({ message: "Hello welcome to my server" })
@@ -8,13 +9,13 @@ app.get('/api/get', (req, res) => {
 app.get('/api/get_user_details', (req, res)=> {
     res.send({
         user: {
-            name:"Satyam",
+            name:process.env.NAME,
             age: 22,
             contact: 122333
         }
     })
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("listen to 3000");
 })
